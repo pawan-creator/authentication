5bd691328c4b4cdc810e04b804a71a7c32f08ace
unknown <pawanmohangg@gmail.com>
2020-10-04 03:11:23 +0530

    signin with googleOauth

--- app.js
+++ app.js
@@ -7,12 +7,15 @@ const mongoose=require("mongoose");
 const session=require("express-session");
 const passport=require("passport");
 const passportLocalMongoose=require("passport-local-mongoose");
+const findOrCreate = require('mongoose-findorcreate');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
 // const md5=require("md5");
 
 // const encrypt=require("mongoose-encryption");
 // const bcrypt=require("bcrypt");
 // const saltRounds=10;
-
+//
+//
 
 const app=express();
 app.use(express.static("public"));
@@ -32,18 +35,48 @@ mongoose.set("useCreateIndex",true);
 
 const userSchema= new mongoose.Schema({
   email:String,
-  password:String
+  password:String,
+  googleId:String
 });
 
 userSchema.plugin(passportLocalMongoose);
+userSchema.plugin(findOrCreate)
+
+
+
 
 // userSchema.plugin(encrypt,{secret:process.env.SECRET,encryptedFields:["password"]});
 
 const User=mongoose.model("User",userSchema);
 
 passport.use(User.createStrategy());
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
+passport.serializeUser(function(user, done) {
+  done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done) {
+  User.findById(id, function(err, user) {
+    done(err, user);
+  });
+});
+// passport.serializeUser(User.serializeUser());
+// passport.deserializeUser(User.deserializeUser());
+
+
+passport.use(new GoogleStrategy({
+    clientID: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    callbackURL: "http://localhost:3000/auth/google/secrets",
+    userProfileURL:"https://www.googleapis.com/oauth2/v3/userinfo"
+  },
+  function(accessToken, refreshToken, profile, cb) {
+    console.log(profile);
+    User.findOrCreate({ googleId: profile.id }, function (err, user) {
+      //here it will find the googleid if it already exists(already registerd to google) or create it if it is not(not registered yet)
+      return cb(err, user);
+    });
+  }
+));
 
 
 
@@ -54,6 +87,18 @@ app.set("view engine","ejs");
 app.get("/",function(req,res){
   res.render("home");
 });
+
+app.get('/auth/google',
+  passport.authenticate('google', { scope: ['profile'] }));   //path to the google sign in page and we are getting google profile of the user
+
+
+
+  app.get("/auth/google/secrets",
+    passport.authenticate('google', { failureRedirect: '/login' }),   //after authenticate it will redirect to the secrets if it fail it will redirect to login
+    function(req, res) {
+
+      res.redirect('/secrets');
+    });
 
 app.get("/login",function(req,res){
   res.render("login");
